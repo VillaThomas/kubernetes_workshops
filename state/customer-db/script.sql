d29e40f95fca0e24957ed45d7bf9b44fe58aa21a
VillaThomas <thomas.villaldea@gmail.com>
2019-09-17 19:45:13 +0200

    Update script.sql

--- state/customer-db/script.sql
+++ state/customer-db/script.sql
@@ -4,4 +4,4 @@ CREATE TABLE customer(
    username VARCHAR (50) UNIQUE NOT NULL,
    name VARCHAR (355) UNIQUE NOT NULL
 );
-INSERT INTO customer(id, username, name) VALUES (1, 'atridou', 'TRIDOU');
+INSERT INTO customer(id, username, name) VALUES (1, 'john', 'DOE');
